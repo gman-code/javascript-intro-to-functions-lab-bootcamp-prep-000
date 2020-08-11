2a58e20f72e84a38b932aad57b7468ede40137d0
Ghamdan Mohsin <chemist2ever@gmail.com>
2020-08-11 02:40:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 function shout(string) {
   return string.toUpperCase()
 }
-function whisper(string){
+function whisper(string) {
   return string.toLowerCase()
+}
+function logShout(string) {
+   console.log('HELLO'. toUpperCase() )
+}
+function logWhisper(string){
+  console.log('hello'. toLowerCase())
+}
+function sayHiToGrandma(string){
+  return string.("I can't hear you!",if `string`is lowercase)
 }
